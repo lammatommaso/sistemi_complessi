95176fe8e427ba26e35779e5bd93d2ef1e53d756
Tommaso Lamma <lammatommaso@gmail.com>
2020-10-27 09:15:33 +0100

    roba

--- src/cpp/road.h
+++ src/cpp/road.h
@@ -4,23 +4,30 @@
 
 class road
 {
-        node A;
+        node _A;
 	//Inizio della strada
-        node B;
+        node _B;
 	//Fine della strada
-	short N;
+	short _N;
 	//Numero massimo di auto nella strada
-	double time;
+	double _time[_N];
 	//Auto in transito nella strada in ordine di arrivo
-	short n;
+	short _n=0;
 	//Numero di auto presenti nella strada
-	
-        //Magari mediato con una distribuzione che non riesco a disegnare
+	bool _oneway;
     public:
+        road(node A,node B,bool oneway);
+	//Costruttore di default
+	//Riempie l'array con N zeri e n=0
         void add_car();
 	//Aggiunge un tempo all'array a 0
 	//n++
 	//Tutti i tempi nell'array fino ad n sono incrementati di continuo 
+	bool is_full()const;
+	//true se n=N
+	double get_time()const;
+	//Media i tempi dell'array con una distibuzione 
+	//pesata maggiormente sugli ultimi che hanno lasciato la strada
 };
 
 
